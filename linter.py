7adc3bb80b9fbc3638200cf272da9f5f4d5ac031
SirReal <jon@surrell.es>
2014-01-22 20:15:43 +0100

    Remove ,,,

--- linter.py
+++ linter.py
@@ -2,8 +2,8 @@
 # linter.py
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
-# Written by Jon Surrell,,,
-# Copyright (c) 2014 Jon Surrell,,,
+# Written by Jon Surrell
+# Copyright (c) 2014 Jon Surrell
 #
 # License: MIT
 #
@@ -18,7 +18,7 @@ class Govet(Linter):
     """Provides an interface to go vet."""
 
     syntax = 'go'
-    cmd = ('go', 'vet')
+    cmd = ('go', 'tool', 'vet')
     regex = r'^vet:\s+.+:\s+.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)$'
     tempfile_suffix = 'go'
     error_stream = util.STREAM_STDERR
