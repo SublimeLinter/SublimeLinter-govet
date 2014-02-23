1c014e6f9496eacb1c482752fc35d2bfd40c55f5
SirReal <jon@surrell.es>
2014-02-23 19:43:44 +0100

    regex fix

--- linter.py
+++ linter.py
@@ -19,6 +19,6 @@ class Govet(Linter):
 
     syntax = ('go', 'gosublime-go')
     cmd = ('go', 'tool', 'vet')
-    regex = r'^vet:\s+.+:\s+.+:(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
+    regex = r'^.+:(?P<line>\d+):\s+(?P<message>.+)'
     tempfile_suffix = 'go'
     error_stream = util.STREAM_STDERR
