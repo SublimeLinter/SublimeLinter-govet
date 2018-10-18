8b173929f251d1c233fc05eb9c66bf69973c5538
Igor Cheledinov <cheledinov@gmail.com>
2018-10-18 23:02:40 +0300

    Adds multiline message support.

--- linter.py
+++ linter.py
@@ -2,9 +2,16 @@ from SublimeLinter.lint import Linter, util
 
 
 class Govet(Linter):
-    cmd = ('go', 'tool', 'vet')
-    regex = r'.+?:(?P<line>\d+):((?P<col>\d+):)?\s+(?P<message>.+)'
-    tempfile_suffix = 'go'
+    cmd = ('go', 'vet', '${file_path}')
+
+    regex = (
+        r'.+?:(?P<line>\d+):((?P<col>\d+):)'  # Parse line and col.
+        r'?\s+(?P<message>(.+\s*(?!(.+\/)))+)'  # Parse multiline message.
+    )
+
+    multiline = True
+
+    tempfile_suffix = '-'
     error_stream = util.STREAM_STDERR
     defaults = {
         'selector': 'source.go'
