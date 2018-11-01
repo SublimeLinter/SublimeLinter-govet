72c61843d6ea124326a67f39cab14f126bcc4bf1
Igor Cheledinov <cheledinov@gmail.com>
2018-11-01 13:24:12 +0300

    Improve regex

--- linter.py
+++ linter.py
@@ -2,14 +2,9 @@ from SublimeLinter.lint import Linter, util
 
 
 class Govet(Linter):
-    cmd = ('go', 'vet', '${file_path}')
+    cmd = ('go', 'vet', '${file_path:file_name}')
 
-    regex = (
-        r'.+?:(?P<line>\d+):((?P<col>\d+):)'  # Parse line and col.
-        r'?\s+(?P<message>(.+\s*(?!(.+\/)))+)'  # Parse multiline message.
-    )
-
-    multiline = True
+    regex = r'(?P<filename>^.+):(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
 
     tempfile_suffix = '-'
     error_stream = util.STREAM_STDERR
