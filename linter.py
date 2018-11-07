a02e9d4b32b6a2bcf1d137417c8dc5ef4ec2dbc3
Willy Zhang <hi@willyzhang.com>
2018-11-07 07:49:49 -0800

    Revert "handle missing imports"

--- linter.py
+++ linter.py
@@ -2,11 +2,9 @@ from SublimeLinter.lint import Linter, util
 
 
 class Govet(Linter):
-    cmd = ('go', 'vet', '${file_path:file_name}')
-
-    regex = r'(?P<filename>^.+):(?P<line>\d+):(?P<col>\d+):\s+(?P<message>.+)'
-
-    tempfile_suffix = '-'
+    cmd = ('go', 'tool', 'vet')
+    regex = r'.+?:(?P<line>\d+):((?P<col>\d+):)?\s+(?P<message>.+)'
+    tempfile_suffix = 'go'
     error_stream = util.STREAM_STDERR
     defaults = {
         'selector': 'source.go'
