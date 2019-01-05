0f5b08e8aa0a0ad1106e217064f8e11da98ebc0d
Igor Cheledinov <cheledinov@gmail.com>
2019-01-06 00:06:11 +0300

    Handle multiline go vet messages.

--- linter.py
+++ linter.py
@@ -2,10 +2,19 @@ from SublimeLinter.lint import Linter, util
 
 
 class Govet(Linter):
-    cmd = ('go', 'tool', 'vet')
-    regex = r'.+?:(?P<line>\d+):((?P<col>\d+):)?\s+(?P<message>.+)'
-    tempfile_suffix = 'go'
+    cmd = ('go', 'vet', '${file_path}')
+
+    regex = r'(?P<filename>^.+?):(?P<line>\d+):((?P<col>\d+):)?\s+(?P<message>(.|\n\t)+)'
+
+    tempfile_suffix = '-'
+    multiline = True
     error_stream = util.STREAM_STDERR
     defaults = {
         'selector': 'source.go'
     }
+
+    def split_match(self, match):
+        if self.filename.find(match.group('filename')) > 0:  # self.filename is an absolute path
+            error = super().split_match(match)
+            flat_message = ' '.join(line.strip() for line in error.message.split('\n'))
+            return error._replace(message=flat_message)
